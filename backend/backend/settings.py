26fd05fe5c020c185dd2606e7a05de83e797a4f4
David Michael <davidlearn27@gmail.com>
2025-09-01 13:53:33 +0100

    Fix API URL formatting in docker-compose.prod.yml and enhance CORS settings in settings.py for improved security and functionality.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -53,9 +53,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -86,14 +86,29 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://159.65.165.21:3000"
 ]
-CORS_ALLOW_METHODS = (
+
+CORS_ALLOW_METHODS = [
     "DELETE",
     "GET",
     "OPTIONS",
     "PATCH",
     "POST",
     "PUT",
-)
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+CORS_ALLOW_CREDENTIALS = True
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
@@ -160,9 +175,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-
-CORS_ALLOW_ALL_ORIGINS = True
 
 
 # settings.py
